161a545690077cd569c8735c7eff5db75d38435f
AlbertAnthonyArrieta <albert.arrieta12@gmail.com>
2023-03-08 11:38:11 -0700

    Work done with svg and interactivity

---
+++ script.js
@@ -0,0 +1,23 @@
+function timeInfo() {
+    document.getElementById("stoneName").innerHTML = "Time Stone"
+}
+
+function soulInfo() {
+    document.getElementById("stoneName").innerHTML = "Soul Stone"
+}
+
+function mindInfo() {
+    document.getElementById("stoneName").innerHTML = "Mind Stone"
+}
+
+function realityInfo() {
+    document.getElementById("stoneName").innerHTML = "Reality Stone"
+}
+
+function spaceInfo() {
+    document.getElementById("stoneName").innerHTML = "Space Stone"
+}
+
+function powerInfo() {
+    document.getElementById("stoneName").innerHTML = "Power Stone"
+}
