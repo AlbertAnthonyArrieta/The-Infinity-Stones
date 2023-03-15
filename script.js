ead4f784391df819ebadc5793cdd43082b27526b
Albert Arrieta <albert.arrieta@uleth.ca>
2023-03-14 21:43:43 -0600

    added some animation

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
 });
 
 function homepage() {
-    $("#gauntletPage").show();
+    $("#gauntletPage").fadeIn("slow");
     $("#tsPage").hide();
     $("#psPage").hide();
     $("#spsPage").hide();
@@ -40,9 +40,9 @@ function homepage() {
     $("#introPage").hide();
     $("#snapPage").hide();
     $("#snapbtn").hide();
-
+    $('.gauntlet__glow').css("fill", "#FFD749");
     
-
+    //Stones
     if (tsC == true) {
         $("#TimeStone").show();
     }
@@ -67,8 +67,52 @@ function homepage() {
         $("#MindStone").show();
     }
 
+    //Hovers
+    $("#tsSlot").hover(function() {
+        $("#tsSlot").css("fill", "#FFD749");
+    }, function(){
+        $("#tsSlot").css("fill", "#DCAC01");
+    });
+
+    $("#psSlot").hover(function() {
+        $("#psSlot").css("fill", "#FFD749");
+    }, function(){
+        $("#psSlot").css("fill", "#DCAC01");
+    });
+
+    $("#spsSlot").hover(function() {
+        $("#spsSlot").css("fill", "#FFD749");
+    }, function(){
+        $("#spsSlot").css("fill", "#DCAC01");
+    });
+
+    $("#rsSlot").hover(function() {
+        $("#rsSlot").css("fill", "#FFD749");
+    }, function(){
+        $("#rsSlot").css("fill", "#DCAC01");
+    });
+
+    $("#ssSlot").hover(function() {
+        $("#ssSlot").css("fill", "#FFD749");
+    }, function(){
+        $("#ssSlot").css("fill", "#DCAC01");
+    });
+
+    $("#msSlot").hover(function() {
+        $("#msSlot").css("fill", "#FFD749");
+    }, function(){
+        $("#msSlot").css("fill", "#DCAC01");
+    });
+
+    //Snap option
     if (tsC == true && psC == true && spsC == true && rsC == true && ssC == true && msC == true) {
         $("#snapbtn").fadeIn(3000);
+        $(".gauntlet__glow").css({
+            "animation": "colorchange 1s",
+            "animation-timing-function": "ease-in-out",
+            "animation-iteration-count": "infinite",
+            "animation-play-state": "running",
+        });
     }
 }
 
@@ -89,45 +133,70 @@ function introPage() {
     
 }
 function timePage() {
+    $("#TimeStone").fadeIn();
+    $(".gauntlet__glow").css("fill", "#05FF00");
+    setTimeout(function() {
+        $("#gauntletPage").fadeOut();
+        $("#tsPage").fadeIn(1000);
+    }, 1000);
     tsC = true;
-    $("#gauntletPage").hide();
-    $("#tsPage").show();
 }
 
 function powerPage() {
+    $("#PowerStone").effect("shake");
+    $("#PowerStone").fadeIn();
+    $(".gauntlet__glow").css("fill", "#BD00FF");
+    setTimeout(function() {
+        $("#gauntletPage").fadeOut();
+        $("#psPage").fadeIn(200);
+    }, 300);
     psC = true;
-    $("#gauntletPage").hide();
-    $("#psPage").show();
 }
 
 function spacePage() {
+    $("#SpaceStone").fadeIn();
+    $(".gauntlet__glow").css("fill", "#0094FF");
+    setTimeout(function() {
+    $("#gauntletPage").fadeOut();
+    $("#spsPage").fadeIn(1000);
+    }, 1000);
     spsC = true;
-    $("#gauntletPage").hide();
-    $("#spsPage").show();
 }
 
 function realityPage() {
+    $("#RealityStone").fadeIn();
+    $(".gauntlet__glow").css("fill", "#FF0000");
+    setTimeout(function() {
+    $("#gauntletPage").fadeOut();
+    $("#rsPage").fadeIn(1000);
+    }, 1000);
     rsC = true;
-    $("#gauntletPage").hide();
-    $("#rsPage").show();
 }
 
 function soulPage() {
+    $("#SoulStone").fadeIn();
+    $(".gauntlet__glow").css("fill", "#FF9900");
+    setTimeout(function() {
+    $("#gauntletPage").fadeOut();
+    $("#ssPage").fadeIn(1000);
+    }, 1000);
     ssC = true;
-    $("#gauntletPage").hide();
-    $("#ssPage").show();
 }
 
 function mindPage() {
+    $("#MindStone").fadeIn();
+    $(".gauntlet__glow").css("fill", "#FAFF00");
+    setTimeout(function() {
+    $("#gauntletPage").fadeOut();
+    $("#msPage").fadeIn(1000);
+    }, 1000);
     msC = true;
-    $("#gauntletPage").hide();
-    $("#msPage").show();
 }
 
 function snapPage() {
-    $("#gauntletPage").hide();
-    $("#snapPage").show();
+    $("#gauntlet").effect("shake");
+    setTimeout(function() {
+        $("#gauntletPage").fadeOut();
+        $("#snapPage").fadeIn();
+    }, 1000);
 }
-
-
-
